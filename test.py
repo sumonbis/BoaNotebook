3031da3c9facdcfe3ae6bcab39da2cba28cf3661
Sumon Biswas <39540376+sumonbis@users.noreply.github.com>
2020-06-28 18:34:24 -0500

    a

--- test.py
+++ test.py
@@ -1,8 +1,1 @@
-for seq_index in range(100):
-    # Take one sequence (part of the training set)
-    # for trying out decoding.
-    input_seq = encoder_input_data[seq_index: seq_index + 1]
-    decoded_sentence = decode_sequence(input_seq)
-    print('-')
-    print('Input sentence:', input_texts[seq_index])
-    print('Decoded sentence:', decoded_sentence)
+x = 10 if a > b else 11
