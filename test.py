6288b8c1f9271180f74476c7d0dc84332375c540
Sumon Biswas <39540376+sumonbis@users.noreply.github.com>
2020-07-12 19:28:56 -0500

    a

--- test.py
+++ test.py
@@ -1,1 +1,6 @@
-h_letters = [ letter for letter in 'human' ]
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+class Msgrpc:
+    def __init__(self, option=[]):
+        self.host = option.get('host') or "127.0.0.1"
+        self.port = option.get('port') or 55552
